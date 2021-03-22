0b6e20b032bbe4c24f23e54dc94dde8e27aaf175
Yahya Saddiq <yahya.saddiq1@vodafone.com>
2021-03-22 10:44:11 +0200

    add widget skeleton

---
+++ Static Widget/WidgetView.swift
@@ -0,0 +1,38 @@
+//
+//  WidgetView.swift
+//  AllahuAkbar
+//
+//  Created by Yahya Saddiq on 3/22/21.
+//
+
+import SwiftUI
+import WidgetKit
+
+struct WidgetData {
+    let nextPrayerTime: String
+    let date: Date
+}
+
+extension WidgetData {
+    static let previewData = WidgetData(
+        nextPrayerTime: ViewModel().nextPrayer?.time ?? "not found",
+        date: Date().advanced(by: (-60*29))
+    )
+}
+
+struct WidgetView: View {
+    let data: WidgetData
+
+    var body: some View {
+        Text(data.nextPrayerTime)
+    }
+}
+
+struct WidgetView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            WidgetView(data: .previewData)
+                .previewContext(WidgetPreviewContext(family: .systemSmall))
+        }
+    }
+}
